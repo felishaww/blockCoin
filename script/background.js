5bfe09b9cbac687d6aa6f715f43018e73909ac48
mira <felishamiranda1510@gmail.com>
2023-06-08 23:34:46 +0700

    remove dynamic detection and improves static analysis function

--- script/background.js
+++ script/background.js
@@ -1,102 +1,88 @@
-/// Declare redirectedTabsnya global
+// Declare redirectedTabs global
 var redirectedTabs = [];
 
-// Function buat detect cryptojacking
-chrome.runtime.onInstalled.addListener(function () {
-  console.log("hey");
-  chrome.storage.sync.get(["whitelist"], function (result) {
-    var whitelist = result.whitelist || [];
+// Load the blacklist from the JSON file
+function loadBlacklist(callback) {
+  fetch(chrome.runtime.getURL("../blacklist.json"))
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.error("Error loading blacklist:", error));
+}
 
-    // Load blacklist dari JSON file
-    function loadBlacklist(callback) {
-      fetch(chrome.runtime.getURL("../blacklist.json"))
-        .then((response) => response.json())
-        .then((data) => callback(data))
-        .catch((error) => console.error("Error loading blacklist:", error));
-    }
-
-    // Cek accessed websitenya ada di blacklist ga
-    function isBlacklisted(url, callback) {
-      loadBlacklist(function (blacklist) {
-        var isBlacklisted = blacklist.includes(url);
-        callback(isBlacklisted);
-      });
-    }
-
-    // Cek apakah  warning page udah open
-    function isWarningPageOpen(tabId, callback) {
-      chrome.tabs.get(tabId, function (tab) {
-        var warningUrl = chrome.runtime.getURL("notification.html");
-        var isOpen = tab.url === warningUrl;
-        callback(isOpen);
-      });
-    }
-
-    chrome.webNavigation.onBeforeNavigate.addListener(
-      function (details) {
-        var url = new URL(details.url);
-        var domain = url.hostname;
-
-        // Check if the extension is disabled
-        chrome.storage.sync.get(["disabled"], function (data) {
-          var isDisabled = data.disabled || false;
-          if (isDisabled) {
-            return;
-          }
-
-          // Check if the accessed website is on the whitelist
-          if (
-            whitelist.some(function (item) {
-              return domain.includes(item);
-            })
-          ) {
-            return;
-          }
-
-          // Check if the warning page is already open
-          isWarningPageOpen(details.tabId, function (isOpen) {
-            if (isOpen) {
-              return;
-            }
-
-            // Check if the accessed website is on the blacklist
-            isBlacklisted(domain, function (blacklisted) {
-              if (blacklisted) {
-                var redirectUrl = chrome.runtime.getURL(
-                  `notification.html?url=${encodeURIComponent(
-                    details.url
-                  )}&domain=${encodeURIComponent(domain)}`
-                );
-                chrome.tabs.update(details.tabId, { url: redirectUrl });
-
-                // Update the blocked count
-                chrome.storage.sync.get(["blockedCount"], function (data) {
-                  var blockedCount = data.blockedCount || 0;
-                  chrome.storage.sync.set({ blockedCount: blockedCount + 1 });
-                });
-
-                // Update the blocked URL history
-                chrome.storage.sync.get(["blockedHistory"], function (data) {
-                  var blockedHistory = data.blockedHistory || [];
-                  blockedHistory.push(details.url);
-                  chrome.storage.sync.set({ blockedHistory: blockedHistory });
-                });
-              } else {
-                // Store the tab ID of the redirected tab
-                redirectedTabs.push(details.tabId);
-
-                // Start CPU monitoring for the redirected tab
-                //startCPUUsageMonitoring(details.tabId);
-              }
-            });
-          });
-        });
-      },
-      {
-        url: [{ schemes: ["http", "https"] }],
-      }
-    );
+// Check if the accessed website is blacklisted
+function isBlacklisted(url, callback) {
+  loadBlacklist(function (blacklist) {
+    var isBlacklisted = blacklist.includes(url);
+    callback(isBlacklisted);
   });
+}
+
+// Function to redirect the tab to the warning page
+function redirectTab(tabId) {
+  var redirectUrl = chrome.runtime.getURL("notification.html");
+  chrome.tabs.update(tabId, { url: redirectUrl });
+}
+
+// Function to redirect the tab to the blocked page
+function blockTab(tabId, url, domain) {
+  var redirectUrl = chrome.runtime.getURL(
+    `notification.html?url=${encodeURIComponent(url)}&domain=${encodeURIComponent(domain)}`
+  );
+  chrome.tabs.update(tabId, { url: redirectUrl });
+
+  // Update the blocked count
+  chrome.storage.sync.get(["blockedCount"], function (data) {
+    var blockedCount = data.blockedCount || 0;
+    chrome.storage.sync.set({ blockedCount: blockedCount + 1 });
+  });
+
+  // Update the blocked URL history
+  chrome.storage.sync.get(["blockedHistory"], function (data) {
+    var blockedHistory = data.blockedHistory || [];
+    blockedHistory.push(url);
+    chrome.storage.sync.set({ blockedHistory: blockedHistory });
+  });
+}
+
+// Function to handle tab navigation
+function handleNavigation(details) {
+  var url = new URL(details.url);
+  var domain = url.hostname;
+
+  // Check if the extension is disabled
+  chrome.storage.sync.get(["disabled"], function (data) {
+    var isDisabled = data.disabled || false;
+    if (isDisabled) {
+      return;
+    }
+
+    // Check if the accessed website is on the whitelist
+    chrome.storage.sync.get(["whitelist"], function (data) {
+      var whitelist = data.whitelist || [];
+      var isWhitelisted = whitelist.some(function (item) {
+        return domain.includes(item);
+      });
+
+      if (isWhitelisted) {
+        return;
+      }
+
+      // Check if the accessed website is on the blacklist
+      isBlacklisted(domain, function (blacklisted) {
+        if (blacklisted) {
+          blockTab(details.tabId, details.url, domain);
+        } else {
+          // Store the tab ID of the redirected tab
+          redirectedTabs.push(details.tabId);
+        }
+      });
+    });
+  });
+}
+
+// Listen for tab navigation events
+chrome.webNavigation.onBeforeNavigate.addListener(handleNavigation, {
+  url: [{ schemes: ["http", "https"] }],
 });
 
 // Message listener from the warning page
@@ -109,151 +95,19 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     }
   } else if (message.type === "whitelist") {
     var domain = message.domain;
+
     // Add the domain to the whitelist
     chrome.storage.sync.get(["whitelist"], function (data) {
       var whitelist = data.whitelist || [];
       whitelist.push(domain);
-      chrome.storage.sync.set({ whitelist: whitelist });
+      chrome.storage.sync.set({ whitelist: whitelist }, function () {
+        // Update the whitelist immediately after it's modified
+        chrome.webNavigation.getAllFrames({ tabId: sender.tab.id }, function (frames) {
+          frames.forEach(function (frame) {
+            handleNavigation({ tabId: sender.tab.id, url: frame.url });
+          });
+        });
+      });
     });
   }
 });
-
-// Function to monitor CPU usage for a specific tab
-function startCPUUsageMonitoring(tabId) {
-  // Calculate the average CPU usage over a period of time
-  function calculateAverageUsage(samples) {
-    var totalUsage = samples.reduce(function (sum, sample) {
-      return sum + sample;
-    }, 0);
-    return totalUsage / samples.length;
-  }
-
-  // Store the CPU usage samples
-  var cpuUsageSamples = [];
-
-  // Monitor CPU usage at regular intervals
-  setInterval(function () {
-    chrome.system.cpu.getInfo(function (cpuInfo) {
-      var totalUsage = cpuInfo.processors.reduce(function (sum, processor) {
-        return sum + processor.usage.total;
-      }, 0);
-      console.log("total usage" + totalUsage);
-      var usagePercentage = (totalUsage * 100) / cpuInfo.processors.length;
-      console.log("usage percentage" + usagePercentage);
-
-      // Add the CPU usage percentage to the samples
-      cpuUsageSamples.push(usagePercentage);
-
-      // Keep only the last 10 samples
-      if (cpuUsageSamples.length > 10) {
-        cpuUsageSamples.shift();
-      }
-
-      // Calculate the average CPU usage
-      var averageUsage = calculateAverageUsage(cpuUsageSamples);
-      console.log(averageUsage + "avg avggg");
-
-      // Check if average usage exceeds 50%
-      if (averageUsage > 50) {
-        // Redirect the tab to the warning page
-        var redirectUrl = chrome.runtime.getURL("notification.html");
-        chrome.tabs.update(tabId, { url: redirectUrl });
-
-        // Remove the redirected tab ID from the redirectedTabs array
-        var index = redirectedTabs.indexOf(tabId);
-        if (index !== -1) {
-          redirectedTabs.splice(index, 1);
-        }
-      }
-    });
-  }, 1000);
-}
-
-// Function to check if a JavaScript file contains cryptojacking libraries
-function checkCryptojackingLibraries(fileContent) {
-  // List of known cryptojacking libraries
-  var cryptojackingLibraries = [
-    "coinhive.com",
-    "cryptoloot.pro",
-    "minero.cc",
-    "miner.start()",
-    "miner.stop()",
-    "Monero",
-    "WebMinePool",
-    "cryptonight",
-    "cryptonight-lite",
-    "cryptonight-jit",
-    "cryptonight.browser",
-    "cryptonight.worker",
-    "javascript-miner",
-    "throttle",
-    "CoinIMP",
-    "crypto",
-    "md5",
-    "sha256",
-    "IntenseCoin",
-    "mybrowsercoins.com",
-    "minemytraffic.com",
-    "mine.js",
-    "coin-hive.com"
-    // Add more cryptojacking libraries here
-  ];
-
-  // Check if the file content contains any cryptojacking library
-  var detectedLibraries = [];
-  cryptojackingLibraries.forEach(function (library) {
-    if (fileContent.includes(library)) {
-      detectedLibraries.push(library);
-    }
-  });
-
-  return detectedLibraries;
-}
-
-// Function to monitor network requests and detect cryptojacking libraries
-function monitorNetworkRequests(tabId) {
-  // Monitor network requests
-  chrome.webRequest.onCompleted.addListener(
-    function (details) {
-      if (details.tabId === tabId && details.type === "script") {
-        // Fetch the file content
-        fetch(details.url)
-          .then((response) => response.text())
-          .then((data) => {
-            // Check if the file contains any cryptojacking library
-            var detectedLibraries = checkCryptojackingLibraries(data);
-            if (detectedLibraries.length > 0) {
-              // Redirect the tab to the warning page
-              var redirectUrl = chrome.runtime.getURL("notification.html");
-              chrome.tabs.update(tabId, { url: redirectUrl });
-
-              // Remove the redirected tab ID from the redirectedTabs array
-              var index = redirectedTabs.indexOf(tabId);
-              if (index !== -1) {
-                redirectedTabs.splice(index, 1);
-              }
-            }
-          })
-          .catch((error) =>
-            console.error("Error fetching file content:", error)
-          );
-      }
-    },
-    { urls: ["<all_urls>"] }
-  );
-}
-
-// Start monitoring CPU usage and network requests for all redirected tabs
-function monitorCPUUsageAndNetworkRequests() {
-  chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      if (redirectedTabs.includes(tabs[i].id)) {
-        //startCPUUsageMonitoring(tabs[i].id);
-        monitorNetworkRequests(tabs[i].id);
-      }
-    }
-  });
-}
-
-// Start monitoring CPU usage and network requests
-//monitorCPUUsageAndNetworkRequests();
